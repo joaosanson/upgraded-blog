858f0b98cfc4c3300edc1c2ac3bf50dc370e5584
João Sanson <joaosanson01@gmail.com>
2022-07-19 17:09:19 -0300

    Just some changes

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+
+
+OWN_EMAIL = "YOUR OWN EMAIL ADDRESS"
+OWN_PASSWORD = "YOUR EMAIL ADDRESS PASSWORD"
 
 app = Flask(__name__)
 
@@ -21,6 +26,25 @@ def post(post_id):
         if blog_post["id"] == post_id:
             current_post = blog_post
     return render_template("post.html", post=current_post)
+
+
+@app.route('/contact', methods=["GET", "POST"])
+def contact():
+    global data
+    if request.method == "POST":
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=OWN_EMAIL, password=OWN_PASSWORD)
+        connection.sendmail(from_addr=OWN_EMAIL,
+                            to_addrs=OWN_EMAIL,
+                            msg=email_message)
 
 
 if __name__ == "__main__":
